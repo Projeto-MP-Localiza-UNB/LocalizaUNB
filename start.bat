0aadfcaef1422825c30bea91d34668d08937ca3e
Gabriel S. Colombo <gabriel.colombo@libertyti.com.br>
2023-11-26 17:50:43 -0300

    Remoção de @REM

--- start.bat
+++ start.bat
@@ -5,7 +5,7 @@ cd client
 call npm install
 start npm start
 
-@REM rem Aguardar alguns segundos para garantir que o servidor do cliente inicie completamente
+rem Aguardar alguns segundos para garantir que o servidor do cliente inicie completamente
 timeout /t 10
 
 rem Iniciar o servidor do backend
