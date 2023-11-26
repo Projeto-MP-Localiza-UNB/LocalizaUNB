659a600fa8ca82c363b5e839792dd5941c68c313
Gabriel S. Colombo <gabriel.colombo@libertyti.com.br>
2023-11-26 17:48:09 -0300

    Inclusão de instalação de dependências

--- start.bat
+++ start.bat
@@ -2,11 +2,13 @@ echo off
 
 rem Iniciar o servidor do cliente
 cd client
+call npm install
 start npm start
 
-rem Aguardar alguns segundos para garantir que o servidor do cliente inicie completamente
+@REM rem Aguardar alguns segundos para garantir que o servidor do cliente inicie completamente
 timeout /t 10
 
 rem Iniciar o servidor do backend
 cd ../server
+call npm install
 start node index.js
