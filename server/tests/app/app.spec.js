0f5e7ada350ed266e709195d29e8fa84f0e6db34
Herisson Nobre <herisson.carvalho96@gmail.com>
2023-11-28 18:03:21 -0300

    Teste do estado da aplicação

---
+++ server/tests/app/app.spec.js
@@ -0,0 +1,12 @@
+import request from "supertest";
+import app from "../../index";
+
+describe("estado da aplicação", () => {
+    it("deve retornar HTTP 200", async () => {
+        // Dispara uma requisição no caminho '/' para a aplicação e armazena a resposta em variável
+        const res = await request(app).get("/");
+
+        // Testa se a propriedade status da resposta é 200 - caso de sucesso
+        expect(res.status).toBe(200);
+    });
+});
