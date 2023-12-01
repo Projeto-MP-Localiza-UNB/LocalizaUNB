3118ea5f500aed9d7074e388e5a0bd8a29c24d03
higorunt <60492449+higorunt@users.noreply.github.com>
2023-12-01 12:25:04 -0300

    Tabela nova Prisma

--- start.bat
+++ start.bat
@@ -1,14 +1,15 @@
 echo off
 
-rem Iniciar o servidor do cliente
-cd client
-call npm install
-start npm start
+rem Iniciar o servidor do backend
+cd server
+start node index.js
 
 rem Aguardar alguns segundos para garantir que o servidor do cliente inicie completamente
-timeout /t 10
+timeout /t 5
 
-rem Iniciar o servidor do backend
-cd ../server
-call npm install
-start node index.js
+rem Iniciar o servidor do cliente
+cd ../client
+start npm start
+
+
+
