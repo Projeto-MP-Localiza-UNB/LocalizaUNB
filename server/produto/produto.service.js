2d2175142c846971a694a60dda7ad1c14e1821d6
Herisson Nobre <herisson.carvalho96@gmail.com>
2023-12-07 18:36:24 -0300

    feat: ajusta branch com novo método

--- server/produto/produto.service.js
+++ server/produto/produto.service.js
@@ -1,23 +1,49 @@
-import {PrismaClient} from "@prisma/client"
+import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient()
+const prisma = new PrismaClient();
 
-class Produto{
-    async criarProduto(nome,descricao,idLoja,imagem){
+class Produto {
+    async criarProduto(nome, descricao, idLoja, imagem) {
         return await prisma.produto.create({
             data: {
                 nome,
                 descricao,
                 imagem,
-                idLoja
+                idLoja,
+            },
+        });
+    }
+
+    async procuraProdutos() {
+        return await prisma.produto.findMany();
+    }
+
+    async excluirProduto(idLoja, idProduto) {
+        try {
+            const produto = await prisma.produto.findFirst({
+                where: {
+                    id: idProduto,
+                    idLoja: idLoja,
+                },
+            });
+
+            if (!produto) {
+                throw new Error(
+                    "Produto não encontrado ou não associado à loja especificada."
+                );
             }
-        })
-    }
+            await prisma.produto.delete({
+                where: {
+                    id: idProduto,
+                    idLoja: idLoja,
+                },
+            });
 
-    async procuraProdutos(){
-        return await prisma.produto.findMany()
+            return "Produto excluído com sucesso.";
+        } catch (error) {
+            return `Erro ao excluir produto: ${error.message}`;
+        }
     }
-
 }
 
-export default Produto
+export default Produto;
