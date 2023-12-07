89d035e34e5bcf59c2c49638cc7b97634b12d9cf
Herisson Nobre <herisson.carvalho96@gmail.com>
2023-12-07 17:42:47 -0300

    refactor: excludes mocks

--- server/jest.config.cjs
+++ server/jest.config.cjs
@@ -2,5 +2,4 @@ module.exports = {
     clearMocks: true,
     testEnvironment: "node",
     testMatch: ["**/__tests__/**/*.js?(x)", "**/?(*.)+(spec|test).js?(x)"],
-    setupFilesAfterEnv: ["./libs/__mocks__/prisma.js"],
 };
