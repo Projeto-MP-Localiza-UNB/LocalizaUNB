93531cea731bf2359bfc6c4da904bd8c7536f359
Herisson Nobre <herisson.carvalho96@gmail.com>
2023-12-09 20:48:58 -0300

    refactor: formatação de código

--- server/produto/produto.controll.js
+++ server/produto/produto.controll.js
@@ -1,25 +1,45 @@
-import Produto from "./produto.service.js"
-import JwtGuard from "../guards/jwt.guard.js"
-import {Router} from "express"
+import Produto from "./produto.service.js";
+import JwtGuard from "../guards/jwt.guard.js";
+import { Router } from "express";
 
-const produtoRotas = Router()
+const produtoRotas = Router();
 
-const produto = new Produto()
+const produto = new Produto();
 
-produtoRotas.post("/produto",JwtGuard, async(enviado,resposta) => {
-    console.log(3)
-    const{nome,descricao,imagem} = enviado.body
-    try{
-        const novoProduto = await produto.criarProduto(nome,descricao,enviado.loja.id,imagem)
-        resposta.status(200).json(novoProduto)
-    }catch(err){
-        resposta.status(400).json({erro: err.message})
+produtoRotas.post("/produto", JwtGuard, async (enviado, resposta) => {
+    console.log(3);
+    const { nome, descricao, imagem } = enviado.body;
+    try {
+        const novoProduto = await produto.criarProduto(
+            nome,
+            descricao,
+            enviado.loja.id,
+            imagem
+        );
+        resposta.status(201).json(novoProduto);
+    } catch (err) {
+        resposta.status(400).json({ erro: err.message });
     }
-})
+});
 
-produtoRotas.get("/produto", async(enviado,resposta) => {
-    const listaProdutos = await produto.procuraProdutos()
-    resposta.status(200).json(listaProdutos)
-})
+produtoRotas.get("/produto", async (enviado, resposta) => {
+    const listaProdutos = await produto.procuraProdutos();
+    resposta.status(200).json(listaProdutos);
+});
 
-export default produtoRotas
+produtoRotas.delete(
+    "/produto/:idLoja/:idProduto",
+    JwtGuard,
+    async (enviado, resposta) => {
+        const { idLoja, idProduto } = enviado.params;
+
+        try {
+            const mensagem = await produto.excluirProduto(idLoja, idProduto);
+            resposta.status(200).json({ mensagem: mensagem });
+        } catch (err) {
+            resposta.status(400).json({ erro: err.message });
+        }
+    }
+);
+
+export default produtoRotas;
