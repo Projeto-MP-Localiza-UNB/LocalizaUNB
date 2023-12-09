6dc6dd5a2f7d50ee1b59db92cb0040cc5e8018f2
higorunt <60492449+higorunt@users.noreply.github.com>
2023-12-09 11:52:17 -0300

    Back-end Documentado
    Documentação JSDOC - Backend

--- server/user/user.service.js
+++ server/user/user.service.js
@@ -4,7 +4,18 @@ import jwt from "jsonwebtoken"
 
 const prisma = new PrismaClient()
 
+/**
+ * Classe que representa um usuário.
+ */
 class User{
+    /**
+     * Cria um novo usuário.
+     * @param {string} nome - O nome do usuário.
+     * @param {string} email - O email do usuário.
+     * @param {string} senha - A senha do usuário.
+     * @returns {Promise<Object>} - Uma promise que resolve com os dados do usuário criado.
+     * @throws {Error} - Se o email já estiver cadastrado.
+     */
     async criarusuario(nome,email,senha){
         const salt = await bcrypt.genSalt()
         senha = await bcrypt.hash(senha, salt) 
@@ -20,9 +31,16 @@ class User{
         })
     }
 
+    /**
+     * Realiza o login de um usuário.
+     * @param {string} email - O email do usuário.
+     * @param {string} senha - A senha do usuário.
+     * @returns {Promise<Object>} - Uma promise que resolve com o token de autenticação.
+     * @throws {Error} - Se o usuário não for encontrado ou a senha estiver incorreta.
+     */
     async entrar(email,senha){
         const user = await prisma.usuario.findUnique({where: { email : email }})
-        if(!user) throw new Error("Usuário não encontrada")
+        if(!user) throw new Error("Usuário não encontrado")
         if(!(await bcrypt.compare(senha, user.senha))) throw new Error ("Senha incorreta")
         const token = jwt.sign({id: user.id},"secret",{expiresIn: "60m"})
         return{token}
