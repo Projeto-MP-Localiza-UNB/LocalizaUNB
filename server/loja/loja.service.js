edcef7fe56bf0e9738a96fef01aa4a19c4011e6e
higorunt <60492449+higorunt@users.noreply.github.com>
2023-12-09 12:14:34 -0300

    Resolve erro em loja

--- server/loja/loja.service.js
+++ server/loja/loja.service.js
@@ -4,46 +4,6 @@ import jwt from "jsonwebtoken"
 
 const prisma = new PrismaClient()
 
-/**
- * Classe que representa uma Loja.
- * @class
- */
-
-    /**
-     * Cadastra uma nova loja.
-     * @param {string} email - O email da loja.
-     * @param {string} nome - O nome da loja.
-     * @param {string} senha - A senha da loja.
-     * @param {string} imagem - A imagem da loja.
-     * @param {number} longitude_fixa - A longitude fixa da loja.
-     * @param {number} latitude_fixa - A latitude fixa da loja.
-     * @returns {Promise<object>} - Uma promise que resolve com os dados da loja cadastrada.
-     * @throws {Error} - Se o email já estiver cadastrado.
-     */
-  
-
-    /**
-     * Realiza o login de uma loja.
-     * @param {string} email - O email da loja.
-     * @param {string} senha - A senha da loja.
-     * @returns {Promise<object>} - Uma promise que resolve com o token de autenticação.
-     * @throws {Error} - Se a loja não for encontrada ou a senha estiver incorreta.
-     */
-   
-
-    /**
-     * Retorna todas as lojas cadastradas.
-     * @returns {Promise<Array<object>>} - Uma promise que resolve com um array contendo todas as lojas cadastradas.
-     */
-   
-
-    /**
-     * Retorna os dados de uma loja específica.
-     * @param {number} id - O ID da loja.
-     * @returns {Promise<object>} - Uma promise que resolve com os dados da loja.
-     * @throws {Error} - Se ocorrer algum erro ao buscar a loja.
-     */
-
 class Loja{
     async cadastroLoja(email,nome,senha,imagem,longitude_fixa,latitude_fixa){
         const salt = await bcrypt.genSalt();
